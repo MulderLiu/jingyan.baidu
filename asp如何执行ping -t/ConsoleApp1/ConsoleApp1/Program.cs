e4281590b9e16b7f22e10889dd76168e80ebc329
Mulder <mulderLiu@outlook.com>
2019-12-14 11:02:21 +0800

    v

---
+++ asp如何执行ping -t/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Process p = new Process())
+            {
+                Console.WriteLine("开始执行。。。。");
+                p.StartInfo.FileName = @"cmd.exe";
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.RedirectStandardInput = true;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.RedirectStandardError = true;
+                p.StartInfo.CreateNoWindow = true;
+                p.Start();//启动程序
+
+                //向cmd窗口写入命令
+                p.StandardInput.WriteLine(@"ping -t 127.0.0.1 > z:\1.txt");
+               // p.StandardInput.WriteLine("exit");
+                p.StandardInput.AutoFlush = true;
+
+                //获取cmd窗口的输出信息
+                string output = p.StandardOutput.ReadToEnd();
+                p.WaitForExit();//等待程序执行完退出进程
+                p.Close();
+                Console.WriteLine(output);
+
+                //Ping ping = new Ping();
+                //            Console.WriteLine(ping.Send("192.168.0.33").Status);
+                //             Console.Read();
+            }
+
+        }
+    }
+}
